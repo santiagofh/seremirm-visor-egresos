df484ee656728949296fd1eaaa1b76e25d3e162c
santiagofh <santiago.farias@gmail.com>
2024-08-05 12:21:32 -0400

    Actualizaci├│n autom├ítica de datos y commit

---
+++ actualizar_seremirm_visor_egresos.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Ejecutar el script de Python
+python "C:\Users\fariass\OneDrive - SUBSECRETARIA DE SALUD PUBLICA\Escritorio\GIE\IEEH-REM20\IEEH - Dashboar de seguimiento\ieeh_descarga_datos.py"
+
+REM Navegar al directorio del repositorio
+cd "C:\Users\fariass\OneDrive - SUBSECRETARIA DE SALUD PUBLICA\Escritorio\GIE\IEEH-REM20\IEEH - Dashboar de seguimiento"
+
+REM Agregar todos los cambios al commit
+git add .
+
+REM Crear un commit con un mensaje
+git commit -m "Actualización automática de datos y commit"
+
+REM Hacer push de los cambios al repositorio remoto
+git push
+
+REM Pausar para que puedas ver el resultado en la consola
+pause
